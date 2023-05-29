4cc383bf88b3370a95be87477b79e0bc54a86aab
Arekusey-tyan <43652041+arekusey-tyan@users.noreply.github.com>
2023-05-29 03:36:48 +0300

    Test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module cpw.mods.bootstraplauncher {
+    uses java.util.function.Consumer;
+    requires java.base;
+    requires cpw.mods.securejarhandler;
+
+    exports cpw.mods.bootstraplauncher;
+}
